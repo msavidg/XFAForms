2d4d15e5c429ca17011eeb573dd0378b2b96d40b
msavidg <msavidg@gmail.com>
2015-08-28 17:20:03 -0400

    Added missing un tracked file.

---
+++ XFAForms.Common/XFAUtilities.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XFAForms.Common
+{
+    public static class XFAUtilities
+    {
+
+        public static string hrefSplitter(string href)
+        {
+
+            string[] tokens = href.Split("#".ToCharArray());
+
+            return tokens[0];
+
+        }
+
+    }
+}
